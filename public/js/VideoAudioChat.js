b0a663bfb12112cb64f26e225d4f9da66b5b339d
longicorn <yusheng531@gmail.com>
2017-12-14 22:40:59 -0500

    new feature
    1. Add hangup and control panel for video chat
    2. Stye change
    3. Add timer for video chat

--- public/js/VideoAudioChat.js
+++ public/js/VideoAudioChat.js
@@ -175,6 +175,17 @@ function createdDescription(description) {
 function gotRemoteStream(event) {
     console.log('got remote stream');
     remoteVideo.src = window.URL.createObjectURL(event.stream);
+
+    $("#videoTimer").toggle();
+    $("#videoControlPanel").toggle();
+
+
+    var sec = 0;
+    function pad ( val ) { return val > 9 ? val : "0" + val; }
+    setInterval( function(){
+        $("#seconds").html(pad(++sec%60));
+        $("#minutes").html(pad(parseInt(sec/60,10)));
+    }, 1000);
 }
 
 function errorHandler(error) {
